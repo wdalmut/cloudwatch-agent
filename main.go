dd7b5badb8ab83b96a4e11a77675d30d5e14bfe1
Walter Dal Mut <walter.dalmut@gmail.com>
2014-06-16 17:22:08 +0200

    Added signal handling for close the daemon

--- main.go
+++ main.go
@@ -1,18 +1,44 @@
 package main
 
 import (
+    "os"
+    "os/signal"
+    "net"
+    "strconv"
+    "strings"
     "github.com/wdalmut/cloudwatch-agent/agent"
 )
 
-func main() {
-    monitorChannel := agent.StartUDPServer("127.0.0.1", 1234)
+const (
+    endpoint = "127.0.0.1"
+    port = 1234
+)
 
+
+func main() {
+    c := make(chan os.Signal, 1)
+    signal.Notify(c, os.Interrupt, os.Kill)
+    agent.W.Add(1)
+    go waitingForKillSignal(c)
+
+    monitorChannel := agent.StartUDPServer(endpoint, port)
     agent.W.Add(1)
     go agent.CollectData(monitorChannel)
-    //Skip wait group
     go agent.SendCollectedData()
 
     agent.W.Wait()
+}
+
+func waitingForKillSignal(c chan os.Signal) {
+    _ = <-c
+
+    agent.L.Info("Kill signal received!")
+
+    addr, _ := net.ResolveUDPAddr("udp", strings.Join([]string{endpoint, strconv.Itoa(port)}, ":"))
+    client,_ := net.DialUDP("udp", nil, addr)
+    client.Write([]byte("close!\n"))
+
+    agent.W.Done()
 }
 
 
