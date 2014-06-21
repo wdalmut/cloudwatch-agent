194bff9b973d716380a5bacedf9a31e447abfb88
Walter Dal Mut <walter.dalmut@gmail.com>
2014-06-21 13:12:44 +0200

    Added base configuration
    The configuration manager uses a JSON configuration file
    that is merged with a default configuration
    
        address     -> 127.0.0.1
        port        -> 1234
        credentials -> env user and key
        loop        -> 60 seconds
    
    The JSON structure should be like
    
        {
            "address": "127.0.0.1",
            "port": 1234,
            "key": "your-key",
            "secret": "your-secret",
            "region": "eu-west-1",
            "loop": 60
        }

---
+++ agent/config.go
@@ -0,0 +1,46 @@
+package agent
+
+import(
+	"encoding/json"
+	"io/ioutil"
+	"os"
+	"io"
+)
+
+type AgentConf struct {
+	Key string
+	Secret string
+	Region string
+	Address string
+	Port int
+	Loop int
+}
+
+func NewConf() (*AgentConf){
+	daemonConf := new(AgentConf)
+	daemonConf.Key = os.Getenv("AWS_ACCESS_KEY_ID")
+
+	daemonConf.Secret = os.Getenv("AWS_SECRET_ACCESS_KEY")
+	if daemonConf.Secret == "" {
+		daemonConf.Secret = os.Getenv("AWS_SECRET_KEY")
+	}
+
+	daemonConf.Region = "eu-west-1"
+	daemonConf.Address = "127.0.0.1"
+	daemonConf.Port = 1234
+	daemonConf.Loop = 60
+
+	return daemonConf
+}
+
+func (d *AgentConf)MergeWithFile(src io.Reader) (error) {
+	file, err := ioutil.ReadAll(src)
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(file, d)
+
+	return err
+}
+
--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
     "strconv"
     "strings"
     "github.com/wdalmut/cloudwatch-agent/agent"
+	"flag"
+	"fmt"
 )
 
 const (
@@ -16,17 +18,40 @@ const (
 
 
 func main() {
+	var confPath string
+
+	flag.StringVar(&confPath, "conf", "", "Local configuration path")
+	flag.Parse()
+
+	conf := agent.NewConf()
+	mergeConfig(conf, confPath)
+
     c := make(chan os.Signal, 1)
     signal.Notify(c, os.Interrupt, os.Kill)
     agent.W.Add(1)
     go waitingForKillSignal(c)
 
-    monitorChannel := agent.StartUDPServer(endpoint, port)
+    monitorChannel := agent.StartUDPServer(conf)
     agent.W.Add(1)
     go agent.CollectData(monitorChannel)
-    go agent.SendCollectedData()
+    go agent.SendCollectedData(conf)
 
     agent.W.Wait()
+}
+
+func mergeConfig(conf *agent.AgentConf, confPath string) {
+	if confPath != "" {
+		src, err := os.Open(confPath)
+		if err == nil {
+			agent.L.Info(fmt.Sprintf("Merge default configuration with file at path: %s", confPath))
+			err = conf.MergeWithFile(src)
+			if err != nil {
+				agent.L.Warning(fmt.Sprintf("Unable to merge file %s, wrong JSON format probably", confPath))
+			}
+		} else {
+			agent.L.Warning(fmt.Sprintf("Missing file at path %s", confPath))
+		}
+	}
 }
 
 func waitingForKillSignal(c chan os.Signal) {
