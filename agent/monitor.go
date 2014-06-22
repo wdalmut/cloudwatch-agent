1146063c8869a77253a8c86301559f38e0d4054f
Walter Dal Mut <walter.dalmut@gmail.com>
2014-06-22 12:42:27 +0200

    Refactor source format

--- agent/monitor.go
+++ agent/monitor.go
@@ -1,88 +1,88 @@
 package agent
 
 import (
-    "sync"
-    "strings"
+	"strings"
+	"sync"
 )
 
 const (
-    OP_AVG = "avg"
-    OP_SUM = "sum"
-    OP_MAX = "max"
-    OP_MIN = "min"
+	OP_AVG = "avg"
+	OP_SUM = "sum"
+	OP_MAX = "max"
+	OP_MIN = "min"
 )
 
 type MetricData struct {
-    Namespace string
-    Metric string
-    Unit string
-    Value float64
-    Op string
+	Namespace string
+	Metric    string
+	Unit      string
+	Value     float64
+	Op        string
 }
 
 type Samples struct {
-    sync.Mutex
-    metrics map[string]*MetricData
+	sync.Mutex
+	metrics map[string]*MetricData
 }
 
-func (h *MetricData)Key() string {
-    return h.Namespace + ":" + h.Metric
+func (h *MetricData) Key() string {
+	return h.Namespace + ":" + h.Metric
 }
 
-func (h *MetricData)Update(point *MetricData) {
-    switch h.Op {
-    default:
-        h.avg(point)
-    case OP_AVG:
-        h.avg(point)
-    case OP_MAX:
-        h.max(point)
-    case OP_MIN:
-        h.min(point)
-    case OP_SUM:
-        h.sum(point)
-    }
+func (h *MetricData) Update(point *MetricData) {
+	switch h.Op {
+	default:
+		h.avg(point)
+	case OP_AVG:
+		h.avg(point)
+	case OP_MAX:
+		h.max(point)
+	case OP_MIN:
+		h.min(point)
+	case OP_SUM:
+		h.sum(point)
+	}
 }
 
-func (h *MetricData)max(point *MetricData) {
-    if point.Value > h.Value {
-        h.Value = point.Value
-    }
+func (h *MetricData) max(point *MetricData) {
+	if point.Value > h.Value {
+		h.Value = point.Value
+	}
 }
 
-func (h *MetricData)min(point *MetricData) {
-    if point.Value < h.Value {
-        h.Value = point.Value
-    }
+func (h *MetricData) min(point *MetricData) {
+	if point.Value < h.Value {
+		h.Value = point.Value
+	}
 }
 
-func (h *MetricData)sum(point *MetricData) {
-    h.Value += point.Value
+func (h *MetricData) sum(point *MetricData) {
+	h.Value += point.Value
 }
 
-func (h *MetricData)avg(point *MetricData) {
-    h.Value += point.Value
-    h.Value /= 2
+func (h *MetricData) avg(point *MetricData) {
+	h.Value += point.Value
+	h.Value /= 2
 }
 
-func (h *Samples)addPoint(data *MetricData) {
-    actualPoint := h.metrics[data.Key()]
+func (h *Samples) addPoint(data *MetricData) {
+	actualPoint := h.metrics[data.Key()]
 
-    if (actualPoint == nil) {
-        actualPoint = new(MetricData)
+	if actualPoint == nil {
+		actualPoint = new(MetricData)
 
-        actualPoint.Metric = data.Metric
-        actualPoint.Namespace = data.Namespace
+		actualPoint.Metric = data.Metric
+		actualPoint.Namespace = data.Namespace
 
-        actualPoint.Value = data.Value
-        actualPoint.Unit = data.Unit
+		actualPoint.Value = data.Value
+		actualPoint.Unit = data.Unit
 
-        actualPoint.Op = strings.ToLower(data.Op)
+		actualPoint.Op = strings.ToLower(data.Op)
 
-        h.metrics[data.Key()] = actualPoint
-    } else {
-        actualPoint.Update(data)
-    }
+		h.metrics[data.Key()] = actualPoint
+	} else {
+		actualPoint.Update(data)
+	}
 }
 
 func collectData(metricPipe chan *MetricData) *Samples {
@@ -90,25 +90,23 @@ func collectData(metricPipe chan *MetricData) *Samples {
 	database := new(Samples)
 	database.metrics = make(map[string]*MetricData)
 
-    W.Add(1)
-    go func() {
-        for {
-            data, ok := <-metricPipe
-            if !ok {
-                L.Info("The metric data pipeline is closed!")
-                break
-            }
+	W.Add(1)
+	go func() {
+		for {
+			data, ok := <-metricPipe
+			if !ok {
+				L.Info("The metric data pipeline is closed!")
+				break
+			}
 
-            database.Lock()
-            database.addPoint(data)
-            database.Unlock()
-        }
+			database.Lock()
+			database.addPoint(data)
+			database.Unlock()
+		}
 
-        L.Info("I close the metric data collection daemon")
-        W.Done()
-    }()
+		L.Info("I close the metric data collection daemon")
+		W.Done()
+	}()
 
 	return database
 }
-
-
--- agent/sender.go
+++ agent/sender.go
@@ -1,79 +1,78 @@
 package agent
 
 import (
-    "time"
-    "fmt"
-    "github.com/crowdmob/goamz/aws"
-    "github.com/crowdmob/goamz/cloudwatch"
+	"fmt"
+	"time"
+
+	"github.com/crowdmob/goamz/aws"
+	"github.com/crowdmob/goamz/cloudwatch"
 )
 
 var cw *cloudwatch.CloudWatch
 var stopTicker chan struct{}
 
 func initCloudWatchAgent(conf *AgentConf) {
-    auth, err := aws.GetAuth(conf.Key, conf.Secret, "",  time.Time{})
-    if err != nil {
-        L.Err(fmt.Sprintf("Unable to create the auth structure for CloudWatch: %v", err))
-        panic(fmt.Sprintf("Unable to create the auth structure for CloudWatch: %v", err))
-    }
+	auth, err := aws.GetAuth(conf.Key, conf.Secret, "", time.Time{})
+	if err != nil {
+		L.Err(fmt.Sprintf("Unable to create the auth structure for CloudWatch: %v", err))
+		panic(fmt.Sprintf("Unable to create the auth structure for CloudWatch: %v", err))
+	}
 
-    region := aws.Regions[conf.Region]
-    cw, err = cloudwatch.NewCloudWatch(auth, region.CloudWatchServicepoint)
-    if err != nil {
-        L.Err(fmt.Sprintf("Unable to login for send statistics: %v", err))
-        panic(fmt.Sprintf("Unable to login for send statistics: %v", err))
-    }
+	region := aws.Regions[conf.Region]
+	cw, err = cloudwatch.NewCloudWatch(auth, region.CloudWatchServicepoint)
+	if err != nil {
+		L.Err(fmt.Sprintf("Unable to login for send statistics: %v", err))
+		panic(fmt.Sprintf("Unable to login for send statistics: %v", err))
+	}
 }
 
 func sendCollectedData(conf *AgentConf, database *Samples) {
-    initCloudWatchAgent(conf)
+	initCloudWatchAgent(conf)
 
-    stopTicker = make(chan struct{})
-    doEvery(time.Duration(conf.Loop) * time.Second, func(time time.Time) {
-        database.Lock()
-        for key, point := range database.metrics {
-            metric := new(cloudwatch.MetricDatum)
+	stopTicker = make(chan struct{})
+	doEvery(time.Duration(conf.Loop)*time.Second, func(time time.Time) {
+		database.Lock()
+		for key, point := range database.metrics {
+			metric := new(cloudwatch.MetricDatum)
 
-            metric.MetricName = point.Metric
-            metric.Timestamp = time
-            metric.Unit = point.Unit
-            metric.Value = point.Value
+			metric.MetricName = point.Metric
+			metric.Timestamp = time
+			metric.Unit = point.Unit
+			metric.Value = point.Value
 
-            metrics := []cloudwatch.MetricDatum{*metric}
+			metrics := []cloudwatch.MetricDatum{*metric}
 
-            if _, err := cw.PutMetricDataNamespace(metrics, point.Namespace); err != nil {
-                L.Err(fmt.Sprintf("%v", err))
-            } else {
-                L.Info("Metric with key: \"" + key + "\" sent to cloud correcly")
-            }
+			if _, err := cw.PutMetricDataNamespace(metrics, point.Namespace); err != nil {
+				L.Err(fmt.Sprintf("%v", err))
+			} else {
+				L.Info("Metric with key: \"" + key + "\" sent to cloud correcly")
+			}
 
-
-            delete(database.metrics, key)
-        }
-        database.Unlock()
-    })
+			delete(database.metrics, key)
+		}
+		database.Unlock()
+	})
 }
 
 func doEvery(every time.Duration, f func(time.Time)) {
-    ticker := time.NewTicker(every)
+	ticker := time.NewTicker(every)
 
-    W.Add(1)
-    go func() {
-        for {
-            select {
-            case <- ticker.C:
-                f(time.Now())
-            case <- stopTicker:
-                ticker.Stop()
+	W.Add(1)
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				f(time.Now())
+			case <-stopTicker:
+				ticker.Stop()
 
-                L.Info("Received stop scheduler, force send operation to CloudWatch")
-                f(time.Now())
-                L.Info("All data sent to CloudWatch, closing data scheduler")
+				L.Info("Received stop scheduler, force send operation to CloudWatch")
+				f(time.Now())
+				L.Info("All data sent to CloudWatch, closing data scheduler")
 
-                W.Done()
-                return
-            }
-        }
-    }()
+				W.Done()
+				return
+			}
+		}
+	}()
 }
-
